bf21dc6c2f52ea8444e2aa421084517a86a393c2
mflood <hermannwest@gmail.com>
2023-04-15 10:23:58 -0600

    ini ios project

---
+++ k-movie-diary/k-movie-diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  k-movie-diary
+//
+//  Created by Matthew Flood on 4/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
